c9ab07ed2b5821fc27e066f42d3af83f2e74ce29
Florian Reimair <office@florianreimair.at>
2023-02-10 16:25:06 +0100

    Add Clip to Page

--- src/Mod/TechDraw/Gui/Command.cpp
+++ src/Mod/TechDraw/Gui/Command.cpp
@@ -861,7 +861,7 @@ void CmdTechDrawClipGroup::activated(int iMsg)
     std::string FeatName = getUniqueObjectName("Clip",page);
     openCommand(QT_TRANSLATE_NOOP("Command", "Create Clip"));
     Gui::cmdAppDocument(page, std::ostringstream() << "addObject('TechDraw::DrawViewClip','" << FeatName << "')");
-    Gui::cmdAppObjectArgs(page, "addView(%s)", getObjectCmd(page));
+    Gui::cmdAppObjectArgs(page, "addView(App.activeDocument().getObject('%s'))", FeatName.c_str());
     updateActive();
     commitCommand();
 }
